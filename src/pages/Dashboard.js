24efce2bd15a88945bb77a9d2c512e0594b640cd
Calvin <calvinchukwuma@gmail.com>
2022-04-07 06:47:47 +0100

    Add files via upload

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+
+
+const Dashboard = ({user}) => {
+
+  return (
+
+    <section className='section'>
+      <h4>Hello, {user?.name} </h4>
+    </section>
+  );
+};
+export default Dashboard;
---
+++ src/pages/SharedProductsLayout.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const SharedProductsLayout = () => {
+  return (
+    <>
+      <h2>Products</h2>
+      <Outlet/>
+    </>
+  )
+}
+
+export default SharedProductsLayout
